f244d3d6d18b9624d7ca8d4abe52a464fba8efea
ashkan <ashkanabdollahi19@gmail.com>
2024-11-05 14:16:55 +0330

    Make client method public in AppStorePurchase class

--- src/Purchase.php
+++ src/Purchase.php
@@ -2,12 +2,14 @@
 
 namespace AppStore\InAppPurchase;
 
+use AshkanAb\AppStore\Client;
 use Illuminate\Support\Facades\Facade;
 
 /**
  *
  * @method static Objects\SubscriptionStatus verifySubscription(string $purchaseTransactionId)
  * @method static Objects\SubscriptionStatus getSubscriptionStatus(string $subscriptionId)
+ * @method static Client client()
  *
  */
 
