c65b78ec4500ad4203106f818ddd6ec5e6cc3e6d
ashkan <ashkanabdollahi19@gmail.com>
2025-02-17 11:59:39 +0330

    Add purchases table stub

--- src/PurchaseServiceProvider.php
+++ src/PurchaseServiceProvider.php
@@ -18,6 +18,14 @@ class PurchaseServiceProvider extends ServiceProvider
                     // you can add any number of migrations here
                 ], 'migrations');
             }
+            // Export the migration
+
+            if (! class_exists('CreatePurchasesTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_purchases_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_purchases_table.php'),
+                    // you can add any number of migrations here
+                ], 'migrations');
+            }
 
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('purchase.php'),
@@ -28,7 +36,7 @@ class PurchaseServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->bind('purchase', function($app) {
+        $this->app->bind('purchase', function() {
             return new AppStorePurchase(config('purchase'));
         });
     }
